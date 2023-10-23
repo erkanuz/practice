46d2961fcda3d4a8d74eda4421360bcbad40c118
erkanuz <erkan.nedim.999@gmail.com>
2023-10-23 20:05:59 +0300

    opps

--- js/script.js
+++ js/script.js
@@ -19,43 +19,6 @@ document.querySelectorAll('.nav_link').forEach(n => n.addEventListener('click', 
 
 /* DARK MODE */
 
-// document.getElementById('theme-button')?.addEventListener('click', () => {
-//     document.body.classList.toggle('dark-mode');
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const moonIcon = document.getElementById('theme-button');
-    
-//     moonIcon?.addEventListener('click', function() {
-//         document.body.classList.toggle('dark-mode');
-//         document.querySelector('.change-theme')?.classList.toggle('ph-moon');
-//         document.querySelector('.change-theme')?.classList.toggle('ph-sun-dim'); 
-//     });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const moonIcon = document.getElementById('theme-button');
-
-//     moonIcon?.addEventListener('click', function() {
-//         const body = document.body;
-//         const icon = document.querySelector('.change-theme');
-
-//         body.classList.toggle('dark-mode');
-//         icon.classList.toggle('ph-moon');
-//         icon.classList.toggle('ph-sun-dim');
-
-//         const isDarkMode = body.classList.contains('dark-mode');
-//         localStorage.setItem('dark-mode', isDarkMode);
-//     });
-
-//     // Check local storage for dark mode preference on page load
-//     const savedDarkMode = localStorage.getItem('dark-mode');
-//     if (savedDarkMode === 'true') {
-//         document.body.classList.add('dark-mode');
-//         document.querySelector('.change-theme').classList.add('ph-sun-dim');
-//     }
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     const moonIcon = document.getElementById('theme-button');
     const icon = document.querySelector('.change-theme');
@@ -85,19 +48,3 @@ const loginContent = document.getElementById('login-content');
 
 addClickEvent(document.getElementById('login-button'), () => loginContent.classList.add('show-login'));
 addClickEvent(document.getElementById('login_close'), () => loginContent.classList.remove('show-login'));
-
-// const loginButton = document.getElementById('login-button'),
-//       loginClose = document.getElementById('login_close'),
-//       loginContent = document.getElementById('login-content')
-
-// if(loginButton) {
-//     loginButton.addEventListener('click', () => {
-//         loginContent.classList.add('show-login')
-//     })
-// }
-
-// if(loginClose) {
-//     loginClose.addEventListener('click', () => {
-//         loginContent.classList.remove('show-login')
-//     })
-// }
